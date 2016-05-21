cc4a4ce01db9e785709a7c67df78edbd48051767
Christos Koutsiaris <koutsiaris@gmail.com>
2016-05-21 20:57:27 +0100

    arduino code

---
+++ flicker/flicker.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(9, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  analogWrite(9, random(120)+135);
+  analogWrite(6, random(120)+135);
+  analogWrite(5, random(120)+135);
+  delay(random(100));
+}
